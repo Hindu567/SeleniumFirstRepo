8b29c3971fe71e0e5c200022922787d781d294c3
hindumathi.matike <hindumathi.matike@HDC3-L-PF1VZNEE.dir.svc.accenture.com>
2023-06-22 18:32:05 +0530

    js

--- src/test/java/ArtOfTesting/JavaScriptExecutorExamples.java
+++ src/test/java/ArtOfTesting/JavaScriptExecutorExamples.java
@@ -10,21 +10,29 @@ public class JavaScriptExecutorExamples {
 
 	public static void main(String[] args) {
 		
-		 WebDriver driver = new FirefoxDriver();
-		 
-		 
-		driver.get("https://www.google.com/");
+		 WebDriver driver = new FirefoxDriver();		 
+		 driver.get("https://demo.guru99.com/test/newtours/");
 		 
 		 JavascriptExecutor js = (JavascriptExecutor) driver;
-		 js.executeScript("window.location='https://www.toolsqa.com/selenium-webdriver/javascript-and-selenium-javascriptexecutor/'");
+		 /*js.executeScript("window.location='https://www.toolsqa.com/selenium-webdriver/javascript-and-selenium-javascriptexecutor/'");
 		 
-		 js.executeScript("location.reload()");
+		// js.executeScript("location.reload()");
 		// js.executeScript("alert(‘hello world’);");
 		// WebElement java = driver.findElement(By.xpath("///button[@class='M6CB1c rr4y5c']"));
 		// js.executeScript("arguments[0].click();", java);
 		 String sText =  js.executeScript("return document.documentElement.innerText;").toString();
 		 String title=js.executeScript("return document.title;").toString();
-		 System.out.println(sText +  title);
+		// System.out.println(sText +  title);
+		 * */
+	// js.executeScript("window.location='https://demo.guru99.com/test/newtours/'");
+		 WebElement uname= driver.findElement(By.name("userName"));
+		 WebElement pwd= driver.findElement(By.name("password"));
+		 js.executeScript("arguments[0].setAttribute('value', arguments[1])", uname, "hindu");
+		 js.executeScript("arguments[0].setAttribute('value', arguments[1])", pwd, "hindu1");
+
+		 WebElement btn= driver.findElement(By.name("submit"));
+		 js.executeScript("arguments[0].click();", btn);
+		 
 		 
 		 
 	}
